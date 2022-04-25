0c9229324c27833bf921dd9a8518850fa8311c0b
mcasa1 <martincasaa@gmail.com>
2022-04-25 19:27:59 -0300

    first commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x25fa5890DFEF123f1E7a910E64fEBe741a649107";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
